4e45e41fe0e022c1091fa5cf9314fc5986d1de9b
POWERSTORM1014 <aigaaru0100@gmail.com>
2023-06-14 05:23:12 +0900

    자학자습안내페이지 구성

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Section1 from './src/screens/Section1'
 import Section2 from './src/screens/Section2'
 import Section3 from './src/screens/Section3'
 import Section4 from './src/screens/Section4'
+import Section5 from './src/screens/Section5'
 
 const Tab = createBottomTabNavigator()
 
@@ -13,10 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Section 1" component={Section1} />
-        <Tab.Screen name="Section 2" component={Section2} />
-        <Tab.Screen name="Section 3" component={Section3} />
-        <Tab.Screen name="Section 4" component={Section4} />
+        <Tab.Screen name="자학자습안내" component={Section1} />
+        <Tab.Screen name="개념강의" component={Section2} />
+        <Tab.Screen name="문제풀이힌트" component={Section3} />
+        <Tab.Screen name="스피드게임" component={Section4} />
+        <Tab.Screen name="쉐도잉훈련" component={Section5} />
       </Tab.Navigator>
     </NavigationContainer>
   )
--- src/screens/Section1.js
+++ src/screens/Section1.js
@@ -1,12 +1,70 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, ImageBackground, Dimensions } from 'react-native'
+import Swiper from 'react-native-swiper'
+
+// Get device window width and height
+const { width, height } = Dimensions.get('window')
 
 export default function Section1() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Section 1!</Text>
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://powerstorm1014.github.io/homepage/assets/images/selfstudy-002.png',
+      }}
+      style={{ width: '100%', height: '100%' }}
+      resizeMode="cover"
+    >
+      <View
+        style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}
+      >
+        <Swiper autoplay={true} style={{ height: 200 }}>
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Image
+              source={{
+                uri: 'https://powerstorm1014.github.io/homepage/assets/images/movie_001.png',
+              }}
+              style={{ width: width, height: height * 0.3 }}
+              resizeMode="cover"
+            />
+          </View>
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Image
+              source={{
+                uri: 'https://powerstorm1014.github.io/homepage/assets/images/hint_001.png',
+              }}
+              style={{ width: width, height: height * 0.3 }}
+              resizeMode="cover"
+            />
+          </View>
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Image
+              source={{
+                uri: 'https://powerstorm1014.github.io/homepage/assets/images/game_001.png',
+              }}
+              style={{ width: width, height: height * 0.3 }}
+              resizeMode="cover"
+            />
+          </View>
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Image
+              source={{
+                uri: 'https://powerstorm1014.github.io/homepage/assets/images/shadowing_001.png',
+              }}
+              style={{ width: width, height: height * 0.3 }}
+              resizeMode="cover"
+            />
+          </View>
+        </Swiper>
+        <Text>Self Study와 함께이기에 가능합니다!!!</Text>
+      </View>
+    </ImageBackground>
   )
 }
-
-//이제 각  섹션 마다 한면씩 입니다. 각각 작성하시면 됩니다.
